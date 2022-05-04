97512484803a43d38b404900293e557f622af956
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-03 22:07:12 -0300

    verique el codigo hice correcciones de codigo

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
+rotulo.innerHTML = "indique la altura de la persona";
+
 btnaltura.addEventListener("click", () => {
   console.log();
+  let dato = document.getElementById("dato");
 
-rotulo.innerHTML = "indique la altura de la persona";
-let dato = document.getElementById("dato");
+  let alturaPermitida: number = 1.3;
+  let alturaPersona: number = dato.value;
 
-let alturaPermitida : number = 1.3;
-let alturaPersona : number = dato.value;
-//let dato : number = Number(dato.value);
-if (alturaPersona <= alturaPermitida) {
-console.log("La persona no puede subir al juego");
-} else {
-console.log("La persona puede subir al juego");
-}}
+  if (alturaPersona <= alturaPermitida) {
+    console.log("La persona no puede subir al juego");
+  } else {
+    console.log("La persona puede subir al juego");
+  }
+});
